96a23136d65fbe1f01b2d50b704d70a6060fe71e
Pablo Rodriguez <arnejado@hotmail.com>
2024-04-13 14:00:20 +0200

    empezamos a crear el objeto botones

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-console.log("script inicializado")
-
 const listaBotones = document.querySelector(".listaBotones");
 const botonMas = document.querySelector(".botonMas");
 const borrar = document.querySelector(".borrar");
 const formulario = document.querySelector(".formulario");
+let botones = [];
+
+
 
 console.log(formulario);
 
@@ -18,13 +19,65 @@ function comprobarLocalStorage () {
     console.log("comprobando...");
 };
 
-function cargandoBotones () {
-    console.log("cargando...");
+// Funciones
+function agregarBoton(e) {
+    e.preventDefault();
+
+    //donde el usuario escribe
+    console.log("agregando boton")
+    const botonNuevo = document.querySelector('#botonNuevo').value;
+
+    // validacion del botón
+/*
+    if (botonNuevo === "") {
+
+        mostrarError ("El botón no puede estar vacío");
+
+        return; // evita que se ejecuten más lineas de codigo (funciona en un if dentro de una función)
+    }
+*/
+    const botonObj = {
+        id: Date.now(),
+        boton : botonNuevo   
+    }
+    //Añadir al arreglo de tweets
+
+    botones = [...botones, botonObj];
+
+    //Una vez agregado vamos a cear el HTML
+
+    // crearHTML ()
+
+    //reiniciar el formulario
+    console.log(botonNuevo)  ;
+    console.log(botones);
 }
 
+
+
+//Mostrar error
+
+function mostrarError (error) {
+    
+    const mensajeError = document.createElement('p');
+    mensajeError.textContent = error;
+    mensajeError.classList.add ('error');
+
+    //vamos a insentarlo en el Contenido
+    const contenido = document.querySelector('#contenido');
+    contenido.appendChild(mensajeError);
+    
+    //elimina el mensaje de error despues de tres segundos
+    setTimeout (() => {
+        mensajeError.remove();
+    },    3000);
+    }
+
+    
 function mostrarAgregar () {
     console.log("mostrando agregar...");
     formulario.classList.toggle("active");
+    listaBotones.classList.toggle("active");
     console.log(formulario);
 }
 
